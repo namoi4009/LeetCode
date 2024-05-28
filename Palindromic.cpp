4b30ed8d3608dfecb32ff8bf2f8b063fec59fbea
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2024-05-28 13:38:32 +0700

    Initial Longest Palindromic Substring

---
+++ Palindromic.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string longestPalindromicSubstring(string s)
+{
+    string subString = "";
+
+    return subString;
+}
+
+int main()
+{
+    string s = "";
+    cout << longestPalindromicSubstring << endl;
+    return 0;
+}
